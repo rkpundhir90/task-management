57b6c8a7e7f3aa00cc464d644d4f11359aca96c2
Rohit Pundhir <rkpundhir90@gmail.com>
2024-09-29 10:56:45 +0530

    Added swagger dependency

--- task-management/api/handler/task_manager.go
+++ task-management/api/handler/task_manager.go
@@ -17,10 +17,20 @@ func NewRegisterTaskHandlers(envConfig *config.EnvironmentConfiguration, r *gin.
 	res := TaskManagementResource{envConfig, taskService}
 
 	// Register all routes with the handler functions
-	r.GET("/task/:taskId/tasks", res.GetTasksByTaskId)
+	r.GET("/tasks/:taskId/tasks", res.GetTasksByTaskId)
 
 }
 
+// GetTasks retrieves all tasks or filters by status, user, or due date
+// @Summary Retrieve tasks
+// @Description Retrieve all tasks or filter tasks by status, user, or due date.
+// @Tags tasks
+// @Produce json
+// @Param status query string false "Status"
+// @Param assigned_to query string false "Assigned user"
+// @Param due_date query string false "Due date"
+// @Success 200 {array} model.TaskInfo
+// @Router /tasks [get]
 func (r TaskManagementResource) GetTasksByTaskId(ctx *gin.Context) {
 	taskId := ctx.Param("taskId")
 	ctx.JSON(http.StatusOK, taskId)
--- task-management/api/router.go
+++ task-management/api/router.go
@@ -3,11 +3,21 @@ package api
 import (
 	"net/http"
 
+	_ "github.com/rkpundhir90/task-management/docs"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rkpundhir90/task-management/task-management/api/handler"
 	"github.com/rkpundhir90/task-management/task-management/app"
 	"github.com/rkpundhir90/task-management/task-management/config"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title Task Management API
+// @version 1.0
+// @description API for managing tasks (CRUD operations).
+// @host localhost:8080
+// @BasePath /v1
 
 // NewRouter sets up the routes and returns the Gin engine
 func NewRouter(a app.Interface, envConfig *config.EnvironmentConfiguration) *gin.Engine {
@@ -17,6 +27,8 @@ func NewRouter(a app.Interface, envConfig *config.EnvironmentConfiguration) *gin
 	router := gin.Default()
 	router.Use(gin.Recovery())
 
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	router.NoRoute(func(ctx *gin.Context) {
 		ctx.JSON(http.StatusServiceUnavailable, gin.H{"error": "Method not available for this route"})
 	})
@@ -24,6 +36,8 @@ func NewRouter(a app.Interface, envConfig *config.EnvironmentConfiguration) *gin
 	v1 := router.Group("/v1")
 
 	handler.NewRegisterTaskHandlers(envConfig, v1, taskInstanceService)
+
+	_ = router.Run(":" + envConfig.ServerPort)
 
 	return router
 }
